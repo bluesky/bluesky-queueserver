0a386a0ae275db87325abf99e97e5224096b4b51
Maksim Rakitin <mrakitin@bnl.gov>
2020-09-22 01:57:33 -0400

    STY: flake8 + isort

--- bluesky_queueserver/server/tests/test_http_server.py
+++ bluesky_queueserver/server/tests/test_http_server.py
@@ -1,16 +1,18 @@
-import sys
-import psutil
-import pytest
 import subprocess
+import sys
+
+import pytest
 import requests
-from bluesky_queueserver.manager.tests.test_general import re_manager
+from xprocess import ProcessStarter
+
 import bluesky_queueserver.server.server as bqss
+from bluesky_queueserver.manager.tests.test_general import \
+    re_manager  # noqa F401
 from bluesky_queueserver.server.server import init_func
 
 SERVER_IP = '0.0.0.0'
 SERVER_PORT = '8080'
 
-from xprocess import ProcessStarter
 
 @pytest.fixture
 def aiohttp_server(xprocess):
@@ -28,6 +30,7 @@ def aiohttp_server(xprocess):
     # Clear the queue after the run:
     subprocess.run('qserver -c clear_queue'.split())
     xprocess.getinfo("aiohttp_server").terminate()
+
 
 @pytest.fixture
 def add_plans_to_queue():
@@ -49,19 +52,19 @@ def _request_to_json(request_type, path, **kwargs):
     return resp
 
 
-def test_http_server_hello_handler(re_manager, aiohttp_server):
+def test_http_server_hello_handler(re_manager, aiohttp_server):  # noqa F811
     resp = _request_to_json('get', '/')
     assert resp['msg'] == 'RE Manager'
     assert resp['n_plans'] == 0
     assert resp['is_plan_running'] is False
 
 
-def test_http_server_queue_view_handler(re_manager, aiohttp_server):
+def test_http_server_queue_view_handler(re_manager, aiohttp_server):  # noqa F811
     resp = _request_to_json('get', '/queue_view')
     assert resp['queue'] == []
 
 
-def test_http_server_add_to_queue_handler(re_manager, aiohttp_server):
+def test_http_server_add_to_queue_handler(re_manager, aiohttp_server):  # noqa F811
     resp1 = _request_to_json('post', '/add_to_queue',
                              json={"plan": {"name": "count",
                                             "args": [["det1", "det2"]]}})
@@ -75,7 +78,7 @@ def test_http_server_add_to_queue_handler(re_manager, aiohttp_server):
     assert resp2['queue'][0] == resp1
 
 
-def test_http_server_pop_from_queue_handler(re_manager, aiohttp_server, add_plans_to_queue):
+def test_http_server_pop_from_queue_handler(re_manager, aiohttp_server, add_plans_to_queue):  # noqa F811
 
     resp1 = _request_to_json('get', '/queue_view')
     assert resp1['queue'] != []

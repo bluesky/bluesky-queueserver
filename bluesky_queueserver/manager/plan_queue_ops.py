44b72c88beb59ec647836987faf2c1cf6914784a
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2020-09-25 13:43:47 -0400

    ENH: move the code communicating with Redis to a separate module

--- bluesky_queueserver/manager/plan_queue_ops.py
+++ bluesky_queueserver/manager/plan_queue_ops.py
@@ -1,13 +1,17 @@
 import aioredis
+import json
 
 
 class PlanQueueOperations:
 
     def __init__(self):
-        self._r_pool = await aioredis.create_redis_pool(
-            'redis://localhost', encoding='utf8')
         self._name_running_plan = "running_plan"
         self._name_plan_queue = "plan_queue"
+        self._r_pool = None
+
+    async def start(self):
+        self._r_pool = await aioredis.create_redis_pool(
+            'redis://localhost', encoding='utf8')
 
     async def delete_pool_entries(self):
         """
@@ -43,7 +47,13 @@ class PlanQueueOperations:
 
     async def get_running_plan_info(self):
         """
-        Read info on the currently running plan from Redis
+        Read info on the currently running plan from Redis.
+
+        Returns
+        -------
+        dict
+            Dictionary representing currently running plan. Empty dictionary if
+            no plan is currently running.
         """
         return json.loads(await self._r_pool.get(self._name_running_plan))
 
@@ -57,33 +67,76 @@ class PlanQueueOperations:
     #                       Plan Queue
     async def get_plan_queue_size(self):
         """
-        Returns the number of plan in the queue
+        Get number of plan in the queue.
+
+        Returns
+        -------
+        int
+            The number of plans in the queue.
         """
         return await self._r_pool.llen(self._name_plan_queue)
 
     async def get_plan_queue(self):
         """
-        Returns the list of all plans in the queue
+        Get the list of all plans in the queue. The first element of the list is the first
+        plan in the queue.
+
+        Returns
+        -------
+        list(dict)
+            The list of plans in the queue. Each plan is represented as a dictionary.
+            Empty list is returned if the queue is empty.
         """
-        return await self._r_pool.lrange(self._name_plan_queue, 0, -1)
+        all_plans_json = await self._r_pool.lrange(self._name_plan_queue, 0, -1)
+        return [json.loads(_) for _ in all_plans_json]
 
     async def pop_first_plan(self):
         """
-        Pop the first plan from the queue
+        Pop the first plan from the queue.
+
+        Returns
+        -------
+        dict or None
+            The first plan in the queue represented as a dictionary or None if the queue is empty.
         """
-        return await self._r_pool.lpop(self._name_plan_queue)
+        plan_json = await self._r_pool.lpop(self._name_plan_queue)
+        plan = json.loads(plan_json) if plan_json is not None else None
+        return plan
 
     async def pop_last_plan(self):
         """
-        Pop the first plan from the queue
+        Pop the last plan from the queue
+
+        Returns
+        -------
+        dict or None
+            The first plan in the queue represented as a dictionary or None if the queue is empty.
         """
-        return await self._r_pool.rpop(self._name_plan_queue)
+        plan_json = await self._r_pool.rpop(self._name_plan_queue)
+        plan = json.loads(plan_json) if plan_json is not None else None
+        return plan
+
+    async def push_plan_to_front_of_queue(self, plan):
+        """
+        Add the plan to the the front of the queue.
+
+        Parameters
+        ----------
+        plan: dict
+            Plan represented as a dictionary of parameters
+        """
+        return await self._r_pool.lpush(self._name_plan_queue, json.dumps(plan))
 
     async def add_plan_to_queue(self, plan):
         """
-        Add the plan to the back of the queue
+        Add the plan to the back of the queue.
+
+        Parameters
+        ----------
+        plan: dict
+            Plan represented as a dictionary of parameters
         """
-        return await self._r_pool.rpush(self._name_plan_queue, plan)
+        return await self._r_pool.rpush(self._name_plan_queue, json.dumps(plan))
 
     async def clear_plan_queue(self):
         """

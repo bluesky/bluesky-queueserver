f3b0e15c81e7f76429d751e7b4ddb1fd459ccd9d
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2020-10-16 18:12:43 -0400

    ENH: example of a decorated plan

--- bluesky_queueserver/profile_collection_sim/99-custom.py
+++ bluesky_queueserver/profile_collection_sim/99-custom.py
@@ -1,7 +1,42 @@
 # flake8: noqa
+from bluesky_queueserver.manager.annotation_decorator import parameter_annotation_decorator
 
 
-def move_then_count():
-    "Move motor1 and motor2 into position; then count det."
-    yield from mv(motor1, 1, motor2, 10)
-    yield from count([det1, det2])
+@parameter_annotation_decorator(
+    {
+        "description": "Move motors into positions; then count dets.",
+        "parameters": {
+            "motors": {
+                "description": "List of motors to be moved into specified positions before the measurement",
+                "annotation": "typing.List[Motors]",
+                "devices": {"Motors": ("motor1", "motor2")},
+            },
+            "detectors": {
+                "descriptions": "Detectors to use for measurement.",
+                "annotation": "typing.List[Detectors]",
+                "devices": {"Detectors": ("det1", "det2", "det3")},
+            },
+            "positions": {
+                "descriptions": "Positions to for motors. The number of positions must be equal "
+                "to the number of the motors.",
+                "annotation": "typing.List[float]",
+            },
+        },
+    }
+)
+def move_then_count(motors, detectors, positions):
+    if not isinstance(motors, (list, tuple)):
+        raise TypeError(f"Parameter 'motors' should be a list or a tuple: type(motors) = {type(motors)}")
+    if not isinstance(detectors, (list, tuple)):
+        raise TypeError(f"Parameter 'detectors' should be a list or a tuple: type(detectors) = {type(detectors)}")
+    if not isinstance(positions, (list, tuple)):
+        raise TypeError(f"Parameter 'positions' should be a list or a tuple: type(positions) = {type(positions)}")
+    if len(motors) != len(positions):
+        raise TypeError(
+            f"The lists of 'motors' and 'positions' should have the same number of elements: "
+            f"len(motors) = {len(motors)}, len(positions) = {len(positions)}"
+        )
+
+    mv_args = [val for tup in zip(motors, positions) for val in tup]
+    yield from mv(*mv_args)
+    yield from count(*detectors)

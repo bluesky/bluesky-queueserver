5e6974284c2b4f67401a92f12b75bcf9b3c60ff8
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2020-10-16 14:02:44 -0400

    ENH: custom plan annotations - tests and comments

--- bluesky_queueserver/manager/annotation_decorator.py
+++ bluesky_queueserver/manager/annotation_decorator.py
@@ -7,6 +7,7 @@ import inspect
 import jsonschema
 import textwrap
 import pprint
+import re
 
 _parameter_annotation_schema = {
     "type": "object",
@@ -116,6 +117,59 @@ def _print_indented_block(text, indent=0, text_width=80):
     return textwrap.indent(text, " " * indent)
 
 
+def _get_enclosed_str(text, prefix, suffix):
+    """
+    Remove prefix and suffix from the string if the string contains both prefix and suffix.
+    """
+    success = False
+    text = text.strip()
+    pattern = f"^{prefix}.+{suffix}$"
+    if re.search(pattern, text):
+        len_prefix = len(prefix) - prefix.count("\\")
+        len_suffix = len(suffix) - suffix.count("\\")
+        text = text[len_prefix:-len_suffix]
+        success = True
+    return text, success
+
+
+def _convert_annotation_to_type(param_annotation):
+    """
+    The purpose of this function is to convert Python parameter annotations to types
+    as they are supposed to appear in docstrings. This function will need to be extended.
+    (E.g. <class 'int'> should be converted to int).
+    """
+    enclosing_cases = (
+        ("<class '", "'>"),
+        ("<enum '", "'>"),
+    )
+    for case in enclosing_cases:
+        param_annotation, success = _get_enclosed_str(param_annotation, case[0], case[1])
+        if success:
+            break
+    return param_annotation
+
+
+def _extract_yield_type(param_annotation):
+    enclosing_cases = (
+        (r"typing.Iterator\[", "]"),
+        (r"typing.Iterable\[", "]"),
+    )
+    success = False
+    for case in enclosing_cases:
+        param_annotation, success = _get_enclosed_str(param_annotation, case[0], case[1])
+        if success:
+            break
+
+    # Special case: typing.Generator(<type>, None, None) - extract <type>
+    if not success:
+        param_annotation, success = _get_enclosed_str(param_annotation, "typing.Generator\[", "]")
+        if success:
+            p_list = param_annotation.split(",")
+            if p_list:
+                param_annotation = p_list[0].strip()
+    return param_annotation
+
+
 def _collect_data_for_docstring(func, annotation):
     """
     Collect data to be printed in docstring. The data is collected from
@@ -174,7 +228,7 @@ def _collect_data_for_docstring(func, annotation):
             if not an and parameters[p_name].annotation != inspect.Parameter.empty:
                 an = str(parameters[p_name].annotation)
 
-            doc_params["parameters"][p_name]["annotation"] = an
+            doc_params["parameters"][p_name]["annotation"] = _convert_annotation_to_type(an)
             doc_params["parameters"][p_name]["description"] = desc
             doc_params["parameters"][p_name]["plans"] = plans
             doc_params["parameters"][p_name]["devices"] = devices
@@ -200,7 +254,9 @@ def _collect_data_for_docstring(func, annotation):
                 if return_annotation != inspect.Signature.empty:
                     an = str(return_annotation)
         doc_params["returns"]["description"] = desc
-        doc_params["returns"]["annotation"] = an
+        if doc_params["is_generator"]:
+            an = _extract_yield_type(an)
+        doc_params["returns"]["annotation"] = _convert_annotation_to_type(an)
 
     return doc_params
 
@@ -221,10 +277,10 @@ def _format_docstring(doc_params):
         Formatted docstring.
     """
     text_width = 80  # Text width for the docsting
-    not_documented_str = "WE ARE SORRY, THE ITEM IS NOT DOCUMENTED YET ..."
+    not_documented_str = "THE ITEM IS NOT DOCUMENTED YET ..."
     tab_size = 4
 
-    doc = f"{doc_params['description']}\n"
+    doc = _print_indented_block(doc_params["description"], indent=0, text_width=text_width)
     # The function may have no parameters
     if doc_params["parameters"]:
         doc += _print_docstring_title("Parameters")
@@ -249,11 +305,11 @@ def _format_docstring(doc_params):
                     s = f"'{device_group_name}': {device_list}"
                     doc += _print_indented_block(s, indent=tab_size * 2, text_width=text_width)
 
-            s = f"Kind: {p['kind']}"
+            s = f"Kind: {p['kind']}."
             doc += _print_indented_block(s, indent=tab_size, text_width=text_width)
 
             if p["default"] is not None:
-                s = f"Default: {p['default']}"
+                s = f"Default: {p['default']}."
                 doc += _print_indented_block(s, indent=tab_size, text_width=text_width)
 
         # Print return value type and description
@@ -264,7 +320,7 @@ def _format_docstring(doc_params):
             offset = 0
             s = doc_params["returns"]["annotation"]
             if s:
-                doc += _print_indented_block(s, indent=tab_size, text_width=text_width)
+                doc += _print_indented_block(s, indent=0, text_width=text_width)
                 offset = 1
 
             s = doc_params["returns"]["description"]
@@ -275,6 +331,88 @@ def _format_docstring(doc_params):
 
 
 def parameter_annotation_decorator(annotation):
+    """
+    The decorator allows to attach a custom description (annotation) to a function or generator function.
+    The decorator also creates a function docstring based on information in the custom annotation passed
+    as a parameter, function signature and standard parameter annotations. If function already has
+    docstring, it is not overwritten. Information from custom annotation always overrides information
+    from standard function parameter annotations.
+
+    The purpose of the custom annotations is provide alternative parameter types that may be used
+    during validation of plan parameters when the parameters contain names of devices or plans
+    instead of references to Ophyd devices and Bluesky plans. The Python standard annotations for
+    function parameters may specify object types, while alternative custom annotations may use string
+    types instead of object types. The custom annotations are also extended to include human-readable
+    description of the function, each parameter and function output that could be used in GUI.
+    Additionally, the custom parameter annotations may include expressions of complex types based
+    on ``typing`` module that may contain 'custom' types that represent groups of device or plan names.
+    Those custom types are converted to enums with sets of attributes determined by the provided
+    lists of plan names and devices on the stage of parameter validation. The type expressions may also
+    be used to create user-friendly plan templates in GUI.
+
+    The following is an example of custom annotation (Python dictionary) with inline comments:
+
+    .. code-block:: python
+
+        {
+            # 'description' is mandatory. Exception will be raised if no description is provided.
+            "description": "Custom annotation with plans and devices.",
+
+            # 'parameters' block is optional. Empty 'parameters' block will also be accepted.
+            # The keys in the parameter block are names of the arguments. The docstring
+            # entries will be generated based on Python function parameter annotation (if provided)
+            # if no custom annotation for the parameter is provided. The parameters listed
+            # in this block must exist in function signature, otherwise an exception will be raised.
+            # Function signature for this device would be:
+            #     func(plan, dwell_time, str_or_int_or_float, devices)
+            # The parameters may have default values, which must be specified as part of the function
+            # definition. The default values are included in the docstring.
+            "parameters": {
+                # Parameter that accepts a plan (unusual, but supported by the decorator).
+                "plan": {
+                    "description": "Parameter that accepts a plans.",
+                    "annotation": "typing.Union[Plan1, typing.List[Plan2]]",
+                    "plans": {
+                        # Here we have two groups of plans. Names of the plans are used as types in
+                        #   'annotation'. The example of annotation above allows to pass one plan
+                        #   from the group 'Plan1' or a list of plans from 'Plan2'.
+                        "Plan1": ("count", "scan", "gridscan"),
+                        "Plan2": ("some", "other", "plans"),
+                    },
+                },
+                "dwell_time": {
+                    "description": "Dwell time.",
+                    "annotation": "float",
+                }
+                "str_or_int_or_float": {
+                    "description": "Some values that may be of 'str', 'int' or 'float' type.",
+                    "annotation": "typing.Union[str, int, float]",
+                }
+                "devices": {
+                    "description": "Parameter that accepts the list of devices.",
+                    "annotation": "typing.List(Device)",
+                    # Here we provide the list of devices. 'devices' and 'plans' are treated
+                    #   similarly, but it may be useful to distinguish lists of plans and devices
+                    #   on the stage of plan parameter validation.
+                    "devices": {"Device": ("det1", "det2", "det3")},
+                },
+            },
+
+            # 'returns' block is optional. Returns block is used both for regular functions and
+            #   generator functions. The created docstring will have 'Yields' or 'Returns' block
+            #   depending on whether the function is a generator. Also if the function is
+            #   a generator, the yield type will be automatically extracted from the expressions
+            #     typing.Iterator[<type>]
+            #     typing.Iterable[<type>]
+            #     typing.Generator[<type>, sometype, sometype]
+            #   and placed in docstring as Yield type.
+            "returns": {
+                "description": "Sequence of number",
+                "annotation": "str"
+            },
+        }
+    """
+
     def function_wrap(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
--- bluesky_queueserver/manager/tests/test_annotation_decorator.py
+++ bluesky_queueserver/manager/tests/test_annotation_decorator.py
@@ -1,42 +1,52 @@
-# import typing
-from bluesky_queueserver.manager.annotation_decorator import parameter_annotation_decorator
+import typing
+import pytest
+from bluesky_queueserver.manager.annotation_decorator import (
+    parameter_annotation_decorator,
+    _convert_annotation_to_type,
+    _extract_yield_type,
+)
 
-"""
-_annotation_dict_1 = {
-    "detector": {
-        "devices": {"Device": ("det1", "det2", "det3")},
-        "annotation": "Device",
-        "description": "Device name",
-    },
-    "detectors": {
-        "devices": {"Device": ("det1", "det4"), "Motor": ("motor10", "motor12")},
-        "annotation": "typing.List[typing.Union[Device, Motor]]",
-        "description": "Device names",
-    },
-    "args": {
-        "devices": {"Motor": ("motor1", "motor2", "motor3"), "Detector": ("det30", "det31")},
-        "annotation": "typing.Union[Detector, Motor, int]",
-        "description": "Motors or ints",
-    },
-    "kwargs": {
-        "devices": {"Detector": ("det50", "det51")},
-        "annotation": "typing.Union[float, Detector]",
-        "description": "Detectors and ints",
+
+# fmt: off
+@pytest.mark.parametrize("original, converted", [
+    ("<class 'int'>", "int"),
+    ("<enum 'SomeEnum'>", "SomeEnum"),
+])
+# fmt: on
+def test_convert_annotation_to_type(original, converted):
+    assert _convert_annotation_to_type(original) == converted
+
+
+# fmt: off
+@pytest.mark.parametrize("original, converted", [
+    ("typing.Iterator[int]", "int"),
+    ("typing.Iterable[int]", "int"),
+    ("typing.Generator[int, None, None]", "int"),
+])
+# fmt: on
+def test_extract_yield_type(original, converted):
+    assert _extract_yield_type(original) == converted
+
+
+_simple_annotation = {
+    "description": "Simple generator function",
+    "parameters": {
+        "val_arg": {"description": "Parameter 'val_arg'"},
+        "val_kwarg": {"description": "Parameter 'val_kwarg'"},
     },
 }
 
+_simple_annotation_doc = """Simple generator function
 
-@parameter_annotation_decorator(_annotation_dict_1)
-def _plan_for_testing(
-    detector: typing.Any,
-    detectors: typing.List[typing.Any],
-    val1: float = 10,
-    *args: int,
-    msg: str = "default_string",
-    val2: typing.Union[int, float] = 6,
-    **kwargs: int,
-) -> str:
-    pass
+Parameters
+----------
+val_arg
+    Parameter 'val_arg'
+    Kind: positional or keyword.
+val_kwarg
+    Parameter 'val_kwarg'
+    Kind: keyword only.
+    Default: 10.
 """
 
 _simple_annotation_with_return = {
@@ -54,65 +64,389 @@ Parameters
 ----------
 val_arg
     Parameter 'val_arg'
-    Kind: positional or keyword
+    Kind: positional or keyword.
 val_kwarg
     Parameter 'val_kwarg'
-    Kind: keyword only
-    Default: 10
+    Kind: keyword only.
+    Default: 10.
 
 Returns
 -------
 Return value
 """
 
+# Note: the types in the following annotation don't match the types in the function
+# parameter annotation on purpose. It allows to verify which types are actually placed
+# in the docstring.
+_simple_annotation_with_types = {
+    "description": "Simple function with type annotations",
+    "parameters": {
+        "val_arg": {"description": "Parameter 'val_arg'", "annotation": "float"},
+        "val_kwarg": {"description": "Parameter 'val_kwarg'", "annotation": "int"},
+    },
+    "returns": {"description": "Return value", "annotation": "float"},
+}
 
-def test_annotation_dectorator_1():
+
+_simple_annotation_with_types_doc = """Simple function with type annotations
+
+Parameters
+----------
+val_arg: float
+    Parameter 'val_arg'
+    Kind: positional or keyword.
+val_kwarg: int
+    Parameter 'val_kwarg'
+    Kind: keyword only.
+    Default: 10.
+
+Returns
+-------
+float
+    Return value
+"""
+
+
+_simple_annotation_long_descriptions = {
+    "description": "Simple function with type annotations and long description. " * 10,
+    "parameters": {
+        "val_arg": {"description": "Long description of the parameter 'val_arg'. " * 10, "annotation": "float"},
+        "val_kwarg": {
+            "description": "Long description of the parameter 'val_kwarg'. " * 10,
+            "annotation": "int",
+        },
+    },
+    "returns": {"description": "Very long description of the return value. " * 10, "annotation": "float"},
+}
+
+
+_simple_annotation_long_descriptions_doc = """Simple function with type annotations and long description. Simple function with
+type annotations and long description. Simple function with type annotations and
+long description. Simple function with type annotations and long description.
+Simple function with type annotations and long description. Simple function with
+type annotations and long description. Simple function with type annotations and
+long description. Simple function with type annotations and long description.
+Simple function with type annotations and long description. Simple function with
+type annotations and long description.
+
+Parameters
+----------
+val_arg: float
+    Long description of the parameter 'val_arg'. Long description of the
+    parameter 'val_arg'. Long description of the parameter 'val_arg'. Long
+    description of the parameter 'val_arg'. Long description of the parameter
+    'val_arg'. Long description of the parameter 'val_arg'. Long description of
+    the parameter 'val_arg'. Long description of the parameter 'val_arg'. Long
+    description of the parameter 'val_arg'. Long description of the parameter
+    'val_arg'.
+    Kind: positional or keyword.
+val_kwarg: int
+    Long description of the parameter 'val_kwarg'. Long description of the
+    parameter 'val_kwarg'. Long description of the parameter 'val_kwarg'. Long
+    description of the parameter 'val_kwarg'. Long description of the parameter
+    'val_kwarg'. Long description of the parameter 'val_kwarg'. Long description
+    of the parameter 'val_kwarg'. Long description of the parameter 'val_kwarg'.
+    Long description of the parameter 'val_kwarg'. Long description of the
+    parameter 'val_kwarg'.
+    Kind: keyword only.
+    Default: 10.
+
+Returns
+-------
+float
+    Very long description of the return value. Very long description of the
+    return value. Very long description of the return value. Very long
+    description of the return value. Very long description of the return value.
+    Very long description of the return value. Very long description of the
+    return value. Very long description of the return value. Very long
+    description of the return value. Very long description of the return value.
+"""
+
+
+# fmt: off
+@pytest.mark.parametrize("custom_annotation, expected_docstring", [
+    (_simple_annotation, _simple_annotation_doc),
+    (_simple_annotation_with_return, _simple_annotation_with_return_doc),
+    (_simple_annotation_with_types, _simple_annotation_with_types_doc),
+    (_simple_annotation_long_descriptions, _simple_annotation_long_descriptions_doc),
+
+])
+# fmt: on
+def test_annotation_dectorator_1(custom_annotation, expected_docstring):
     """
     Basic test: decorator is applied to a simple function
     """
 
-    @parameter_annotation_decorator(_simple_annotation_with_return)
+    @parameter_annotation_decorator(custom_annotation)
     def func(val_arg, *, val_kwarg=10):
-        return val_arg + val_kwarg
+        return int(val_arg + val_kwarg)
 
-    assert func._custom_parameter_annotation_ == _simple_annotation_with_return
+    assert func._custom_parameter_annotation_ == custom_annotation
     assert func(10, val_kwarg=20) == 30
-    assert func.__doc__ == _simple_annotation_with_return_doc
+    assert func.__name__ == "func"
+    assert func.__doc__ == expected_docstring
 
 
-_simple_annotation = {
-    "description": "Simple generator function",
+# fmt: off
+@pytest.mark.parametrize("custom_annotation, expected_docstring", [
+    (_simple_annotation_with_types, _simple_annotation_with_types_doc),
+    (_simple_annotation_long_descriptions, _simple_annotation_long_descriptions_doc),
+
+])
+# fmt: on
+def test_annotation_dectorator_2(custom_annotation, expected_docstring):
+    """
+    Types are specified in the custom annotation and in Python parameter annotations.
+    Python annotations are not used.
+    """
+
+    @parameter_annotation_decorator(custom_annotation)
+    def func(val_arg: int, *, val_kwarg: float = 10):
+        return int(val_arg + val_kwarg)
+
+    assert func._custom_parameter_annotation_ == custom_annotation
+    assert func(10, val_kwarg=20) == 30
+    assert func.__name__ == "func"
+    # assert False, print(func.__doc__)
+    assert func.__doc__ == expected_docstring
+
+
+_simple_annotation_no_types = {
+    "description": "Simple function with type annotations",
     "parameters": {
         "val_arg": {"description": "Parameter 'val_arg'"},
         "val_kwarg": {"description": "Parameter 'val_kwarg'"},
     },
+    "returns": {"description": "Return value"},
 }
 
-_simple_annotation_doc = """Simple generator function
+
+_simple_annotation_no_types_doc = """Simple function with type annotations
 
 Parameters
 ----------
-val_arg
+val_arg: int
     Parameter 'val_arg'
-    Kind: positional or keyword
-val_kwarg
+    Kind: positional or keyword.
+val_kwarg: float
     Parameter 'val_kwarg'
-    Kind: keyword only
-    Default: 10
+    Kind: keyword only.
+    Default: 10.
+
+Returns
+-------
+int
+    Return value
 """
 
 
-def test_annotation_dectorator_2():
+# fmt: off
+@pytest.mark.parametrize("custom_annotation, expected_docstring", [
+    (_simple_annotation_no_types, _simple_annotation_no_types_doc),
+])
+# fmt: on
+def test_annotation_dectorator_3(custom_annotation, expected_docstring):
+    """
+    Types are specified in the custom annotation and in Python parameter annotations.
+    Python annotations are not used.
+    """
+
+    @parameter_annotation_decorator(custom_annotation)
+    def func(val_arg: int, *, val_kwarg: float = 10) -> int:
+        return int(val_arg + val_kwarg)
+
+    assert func._custom_parameter_annotation_ == custom_annotation
+    assert func(10, val_kwarg=20) == 30
+    assert func.__name__ == "func"
+    assert func.__doc__ == expected_docstring
+
+
+# fmt: off
+@pytest.mark.parametrize("custom_annotation, expected_docstring", [
+    (_simple_annotation, _simple_annotation_doc),
+])
+# fmt: on
+def test_annotation_dectorator_4(custom_annotation, expected_docstring):
     """
     Basic test: decorator is applied to a generator function.
     """
 
-    @parameter_annotation_decorator(_simple_annotation)
+    @parameter_annotation_decorator(custom_annotation)
     def func(val_arg, *, val_kwarg=10):
         n_elements = val_arg + val_kwarg
         for n in range(n_elements):
             yield n
 
-    assert func._custom_parameter_annotation_ == _simple_annotation
+    assert func._custom_parameter_annotation_ == custom_annotation
     assert list(func(10, val_kwarg=20)) == list(range(30))
-    assert func.__doc__ == _simple_annotation_doc
+    assert func.__name__ == "func"
+    assert func.__doc__ == expected_docstring
+
+
+_custom_annotation_plans_and_devices = {
+    "description": "Custom annotation with plans and devices.",
+    "parameters": {
+        "val_arg": {
+            "description": "Parameter that accepts a plans.",
+            "annotation": "typing.Union(Plan1, Plan2)",
+            "plans": {"Plan1": ("count", "scan", "gridscan"), "Plan2": ("some", "other", "plans")},
+        },
+        "val_kwarg": {
+            "description": "Parameter that accepts the list of devices.",
+            "annotation": "typing.List(Device)",
+            "devices": {"Device": ("det1", "det2", "det3")},
+        },
+    },
+    "returns": {"description": "Sequence of number"},
+}
+
+
+_custom_annotation_plans_and_devices_doc = """Custom annotation with plans and devices.
+
+Parameters
+----------
+val_arg: typing.Union(Plan1, Plan2)
+    Parameter that accepts a plans.
+    Allowed plan names:
+        'Plan1': ('count', 'scan', 'gridscan')
+        'Plan2': ('some', 'other', 'plans')
+    Kind: positional or keyword.
+val_kwarg: typing.List(Device)
+    Parameter that accepts the list of devices.
+    Allowed device names:
+        'Device': ('det1', 'det2', 'det3')
+    Kind: keyword only.
+
+Yields
+------
+int
+    Sequence of number
+"""
+
+
+# fmt: off
+@pytest.mark.parametrize("custom_annotation, expected_docstring", [
+    (_custom_annotation_plans_and_devices, _custom_annotation_plans_and_devices_doc),
+])
+# fmt: on
+def test_annotation_dectorator_5(custom_annotation, expected_docstring):
+    """
+    Test if the list of plans and devices is displayed correctly in the docstring.
+    """
+
+    @parameter_annotation_decorator(custom_annotation)
+    def func(val_arg, *, val_kwarg) -> typing.Iterable[int]:
+        # The following code has no purpose except to see if the generator is still working.
+        added_strings = val_arg + val_kwarg
+        for n in range(len(added_strings)):
+            yield n
+
+    assert func._custom_parameter_annotation_ == custom_annotation
+    assert list(func("gridscan", val_kwarg="det2")) == list(range(len("gridscan") + len("det2")))
+    ## assert False, print(func.__doc__)
+    assert func.__name__ == "func"
+    assert func.__doc__ == expected_docstring
+
+
+_more_complicated_annotation = {
+    "description": "Example of annotation with varargs and varkwargs.",
+    "parameters": {
+        "detector": {
+            "devices": {"Device": ("det1", "det2", "det3")},
+            "annotation": "Device",
+            "description": "Device name",
+        },
+        "detectors": {
+            "devices": {"Device": ("det1", "det4"), "Motor": ("motor10", "motor12")},
+            "annotation": "typing.List[typing.Union[Device, Motor]]",
+            "description": "Device names",
+        },
+        "args": {
+            "devices": {"Motor": ("motor1", "motor2", "motor3"), "Detector": ("det30", "det31")},
+            "annotation": "typing.Union[Detector, Motor, int]",
+            "description": "Motors or ints",
+        },
+        "kwargs": {
+            "devices": {"Detector": ("det50", "det51")},
+            "annotation": "typing.Union[float, Detector]",
+            "description": "Detectors and ints",
+        },
+    },
+    "returns": {"description": "Yields strings from the list of three strings."},
+}
+
+_more_complicated_annotation_doc = """Example of annotation with varargs and varkwargs.
+
+Parameters
+----------
+detector: Device
+    Device name
+    Allowed device names:
+        'Device': ('det1', 'det2', 'det3')
+    Kind: positional or keyword.
+detectors: typing.List[typing.Union[Device, Motor]]
+    Device names
+    Allowed device names:
+        'Device': ('det1', 'det4')
+        'Motor': ('motor10', 'motor12')
+    Kind: positional or keyword.
+val1: float
+    THE ITEM IS NOT DOCUMENTED YET ...
+    Kind: positional or keyword.
+    Default: 10.
+args: typing.Union[Detector, Motor, int]
+    Motors or ints
+    Allowed device names:
+        'Motor': ('motor1', 'motor2', 'motor3')
+        'Detector': ('det30', 'det31')
+    Kind: var positional.
+msg: str
+    THE ITEM IS NOT DOCUMENTED YET ...
+    Kind: keyword only.
+    Default: 'default_string'.
+val2: typing.Union[int, float]
+    THE ITEM IS NOT DOCUMENTED YET ...
+    Kind: keyword only.
+    Default: 6.
+kwargs: typing.Union[float, Detector]
+    Detectors and ints
+    Allowed device names:
+        'Detector': ('det50', 'det51')
+    Kind: var keyword.
+
+Yields
+------
+str
+    Yields strings from the list of three strings.
+"""
+
+# fmt: off
+@pytest.mark.parametrize("custom_annotation, expected_docstring", [
+    (_more_complicated_annotation, _more_complicated_annotation_doc),
+])
+# fmt: on
+def test_annotation_dectorator_6(custom_annotation, expected_docstring):
+    """
+    An example of more complicated docstring with varargs and varkwargs. Some of the
+    args and kwargs are not documented.
+    """
+
+    @parameter_annotation_decorator(custom_annotation)
+    def func(
+        detector: typing.Any,
+        detectors: typing.List[typing.Any],
+        val1: float = 10,
+        *args: int,
+        msg: str = "default_string",
+        val2: typing.Union[int, float] = 6,
+        **kwargs: int,
+    ) -> typing.Generator[str, None, None]:
+        str_list = ["str1", "str2", "str3"]
+        for s in str_list:
+            yield s
+
+    assert func._custom_parameter_annotation_ == custom_annotation
+    assert list(func("det1", ["det1", "det2"])) == ["str1", "str2", "str3"]
+    ##assert False, print(func.__doc__)
+    assert func.__name__ == "func"
+    assert func.__doc__ == expected_docstring

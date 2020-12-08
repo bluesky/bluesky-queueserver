fcbb9c68bf25793f738c945268877b1ffdc3caab
Dmitri Gavrilov <46980826+dmgav@users.noreply.github.com>
2020-12-08 17:49:26 -0500

    Update bluesky_queueserver/manager/qserver_cli.py
    Co-authored-by: Maksim Rakitin <mrakitin@users.noreply.github.com>

--- bluesky_queueserver/manager/qserver_cli.py
+++ bluesky_queueserver/manager/qserver_cli.py
@@ -56,7 +56,7 @@ qserver queue add instruction 2 <instruction>          # Insert instruction at p
 qserver queue add plan -1 '<plan-params>'              # Insert plan at position -1
 qserver queue add plan before '<uid>' '<plan-params>'  # Insert the plan before the plan with given UID
 qserver queue add plan after '<uid>' '<plan-params>'   # Insert the plan after the plan with given UID
-NOTE: Position indexes are 0-based. Inserting a plan to position 0 pushes it to the font of the queue.
+NOTE: Position indices are 0-based. Inserting a plan to position 0 pushes it to the front of the queue.
       Negative position indexes are counted from the back of the queue. Request for a plan with index -1
       returns the last plan of the queue. Inserting a plan at position -1 makes it previous to last.
 

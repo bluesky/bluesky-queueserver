548b8807c3d54680b70aa0d452d4c11ac5f631e0
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2021-03-04 08:27:19 -0500

    ENH: placeholder for 'queue_item_update' API

--- bluesky_queueserver/manager/manager.py
+++ bluesky_queueserver/manager/manager.py
@@ -982,6 +982,11 @@ class RunEngineManager(Process):
 
         return rdict
 
+    async def _queue_item_update_handler(self, request):
+        success, msg, qsize, uid = True, "", 0, "some-uid"
+        rdict = {"success": success, "msg": msg, "qsize": qsize, "uid": uid}
+        return rdict
+
     async def _queue_item_get_handler(self, request):
         """
         Returns an item from the queue. The position of the item
@@ -1271,7 +1276,7 @@ class RunEngineManager(Process):
             "environment_close": "_environment_close_handler",
             "environment_destroy": "_environment_destroy_handler",
             "queue_item_add": "_queue_item_add_handler",
-            # "queue_item_replace": "_queue_item_replace_handler",
+            "queue_item_update": "_queue_item_update_handler",
             "queue_item_get": "_queue_item_get_handler",
             "queue_item_remove": "_queue_item_remove_handler",
             "queue_item_move": "_queue_item_move_handler",

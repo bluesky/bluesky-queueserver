bb60976640d00a7826dbbd3639b79396cdb7f69e
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2021-12-28 10:21:11 -0500

    TST: unit tests for 'qserver task load result'

--- bluesky_queueserver/manager/tests/test_qserver_cli.py
+++ bluesky_queueserver/manager/tests/test_qserver_cli.py
@@ -1216,6 +1216,20 @@ def test_function_execute_2_fail(re_manager):  # noqa: F811
     assert subprocess.call(["qserver", "function", "execute", item, "invalid_param"]) == PARAM_ERROR
 
 
+def test_task_load_result_1(re_manager):  # noqa: F811
+    """
+    Tests for 'qserver task load result'.
+    """
+    # The request should be successful for any 'task_uid'.
+    task_uid = "01e80342-5e36-44de-bc86-9bd8d57c9885"
+    assert subprocess.call(["qserver", "task", "load", "result", task_uid]) == SUCCESS
+
+    # Some cases of invalid parameters
+    assert subprocess.call(["qserver", "task", "load", "result"]) == PARAM_ERROR
+    assert subprocess.call(["qserver", "task", "load", "something"]) == PARAM_ERROR
+    assert subprocess.call(["qserver", "task", "something", "something"]) == PARAM_ERROR
+
+
 _sample_trivial_plan1 = """
 def trivial_plan_for_unit_test():
     '''

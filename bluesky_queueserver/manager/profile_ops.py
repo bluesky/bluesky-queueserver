6d2513758735e5a29f306f42da242fde97d99a4e
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2023-07-19 19:05:09 -0400

    FIX: deprecated 'pkg_resources'

--- bluesky_queueserver/manager/profile_ops.py
+++ bluesky_queueserver/manager/profile_ops.py
@@ -17,7 +17,7 @@ import typing
 from collections.abc import Iterable
 
 import jsonschema
-import pkg_resources
+import importlib.resources
 import pydantic
 import yaml
 from numpydoc.docscrape import NumpyDocString
@@ -173,7 +173,7 @@ def get_default_startup_dir():
     Returns the path to the default profile collection that is distributed with the package.
     The function does not guarantee that the directory exists. Used for demo with Python-based worker.
     """
-    pc_path = pkg_resources.resource_filename("bluesky_queueserver", "profile_collection_sim/")
+    pc_path = os.path.join(importlib.resources.files("bluesky_queueserver"), "profile_collection_sim", "")
     return pc_path
 
 

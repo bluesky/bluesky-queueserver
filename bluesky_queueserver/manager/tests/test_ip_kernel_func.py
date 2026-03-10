187d46c2dad96a7a6cfdaac9c594d2992e600f8a
Jakub Wlodek <jwlodek.dev@gmail.com>
2026-03-10 10:22:48 -0400

    Add missing import

--- bluesky_queueserver/manager/tests/test_ip_kernel_func.py
+++ bluesky_queueserver/manager/tests/test_ip_kernel_func.py
@@ -4,6 +4,7 @@ import time as ttime
 import pytest
 
 from .common import ip_kernel_simple_client  # noqa: F401
+from .common import re_manager_factory  # noqa: F401
 from .common import re_manager  # noqa: F401
 from .common import re_manager_cmd  # noqa: F401
 from .common import (
